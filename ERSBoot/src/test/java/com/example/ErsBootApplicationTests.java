1629d3a3a8b544ac79c2721e423aa2fb6cf69c40
elmcgill <ethanlmcgill@gmail.com>
2022-12-08 14:59:48 -0600

    updated the test

--- ERSBoot/src/test/java/com/example/ErsBootApplicationTests.java
+++ ERSBoot/src/test/java/com/example/ErsBootApplicationTests.java
@@ -1,9 +1,13 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
+@ActiveProfiles("test")
 class ErsBootApplicationTests {
 
 	@Test
